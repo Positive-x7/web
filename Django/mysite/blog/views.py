8ac3f0fb76fb41a9fb161abf47d76a57ea320fa0
Positive-x7 <soltagireevt@gmail.com>
2025-02-22 11:35:48 +0300

    Add files via upload

--- Django/mysite/blog/views.py
+++ Django/mysite/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
-# Create your views here.
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = '<html lang="en"><h1>It is now %s.</h1></html>' % now
+    return HttpResponse(html)
+
+def index(request):
+    return render(request , "blog/index.html", {})
+
+def cat(request, catid):
+    return HttpResponse(f"<h1>Cats {catid} </h1>")
